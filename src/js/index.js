2f8e98a034c6ccc4b0f7197fe5ff7607c9ba7d75
brayanmm6 <martinsbrayan514@gmail.com>
2023-07-25 19:31:38 -0300

    criado repositótio para Quest de Js intermediátio do Dev-quest

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const inputAction = document.querySelectorAll(".input-action")
+const submitButton = document.getElementById("submit-button")
+const errorMessage = document.querySelectorAll(".error-message")
+
+inputAction.forEach((itemInput, index) =>{
+    itemInput.addEventListener("change", function(){
+        if(itemInput.value.length > 0){
+            itemInput.classList.add("border-green")
+        }else{
+            itemInput.classList.remove("border-green")
+        }
+    })
+
+    submitButton.addEventListener("click", function(){
+        if(itemInput.value.length <= 0){
+            errorMessage[index].classList.remove("hide-error-message")
+            itemInput.classList.add("border-red")
+        }else{
+            errorMessage[index].classList.add("hide-error-message")
+            itemInput.classList.remove("border-red")
+        }
+    })
+})
