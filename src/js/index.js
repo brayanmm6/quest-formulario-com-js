571d94046c4ba0c06acec0b304d185124fc344b3
brayanmm6 <martinsbrayan514@gmail.com>
2023-08-01 21:29:07 -0300

    Melhorada a responsividade do meu projeto

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,15 @@ const errorMessage = document.querySelectorAll(".error-message")
 
 inputAction.forEach((itemInput, index) =>{
     itemInput.addEventListener("change", function(){
-        if(itemInput.value.length > 0){
+        if(itemInput.value){
             itemInput.classList.add("border-green")
         }else{
             itemInput.classList.remove("border-green")
         }
     })
 
-    submitButton.addEventListener("click", function(){
+    submitButton.addEventListener("click", function(e){
+        e.preventDefault()
         if(itemInput.value.length <= 0){
             errorMessage[index].classList.remove("hide-error-message")
             itemInput.classList.add("border-red")
